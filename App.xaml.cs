e81b0c87f8fc9ace37385ca06f3ca0b9f742f0bd
Emiliano <emilianovolk75@gmail.com>
2024-05-20 17:43:05 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Week_8_Junk_Notes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
